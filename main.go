aae863a1c803bb1255bfe47333471816bdbc769f
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-07-29 00:05:24 +0100

    refactor basic server | separated saluer func from main

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/saluer", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprint(writer, "Bonjour le monde!")
-	})
+	// Routes
+	http.HandleFunc("/saluer", saluer)
+	//  starts server 1n port 8081
+	log.Fatal(http.ListenAndServe("localhost:8081", nil))
+}
 
-	http.ListenAndServe("localhost:8081", nil)
+func saluer(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprint(writer, "Bonjour le monde!")
 }

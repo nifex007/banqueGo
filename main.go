89772d109806d93a6c9808af6a17cf4203ca550d
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-07-29 01:56:59 +0100

    Obtenir l'itinéraire de tous les clients

--- main.go
+++ main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name string
+	City string
+	Zipcode string
+}
+
 func main() {
 	// Routes
 	http.HandleFunc("/saluer", saluer)
-	//  starts server 1n port 8081
+	http.HandleFunc("/clients", listerLesClients)
+	//  starts server in port 8081
 	log.Fatal(http.ListenAndServe("localhost:8081", nil))
 }
 
 func saluer(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprint(writer, "Bonjour le monde!")
+}
+
+func listerLesClients(writer http.ResponseWriter, request *http.Request) {
+	customers := []Customer{
+		{Name: "John", City: "Paris", Zipcode: "75008"},
+		{Name: "Mary", City: "London", Zipcode: "W1A 0AX"},
+	}
+
+	json.NewEncoder(writer).Encode(customers)
 }

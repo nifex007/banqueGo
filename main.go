12cb62d3d8a8f8e301dd7df8748eff6272a3fb59
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-08-01 01:11:32 +0100

    xml/clients route pour renvoyer l'équivalent xml de la réponse json du client

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 type Customer struct {
-	Name    string `json:"full_name"`
-	City    string `json:"city"`
-	Zipcode string `json:"zipcode"`
+	Name    string `json:"full_name" xml:"full_name"`
+	City    string `json:"city" xml:"city"`
+	Zipcode string `json:"zipcode" xml:"zipcode"`
 }
 
 func main() {
@@ -18,6 +19,7 @@ func main() {
 	// Routes
 	http.HandleFunc("/saluer", saluer)
 	http.HandleFunc("/clients", listerLesClients)
+	http.HandleFunc("/xml/clients", listerLesClientsXML)
 	//  starts server in port 8081
 	fmt.Println("localhost @ "+port)
 	log.Fatal(http.ListenAndServe("localhost"+port, nil))
@@ -35,6 +37,15 @@ func listerLesClients(writer http.ResponseWriter, request *http.Request) {
 	}
 
 	writer.Header().Set("Content-Type", "application/json")
-
 	json.NewEncoder(writer).Encode(customers)
+}
+
+func listerLesClientsXML(writer http.ResponseWriter, request *http.Request){
+	customers := []Customer{
+		{Name: "John", City: "Paris", Zipcode: "75008"},
+		{Name: "Mary", City: "London", Zipcode: "W1A 0AX"},
+	}
+
+	writer.Header().Set("Content-Type", "application/xml")
+	xml.NewEncoder(writer).Encode(customers)
 }

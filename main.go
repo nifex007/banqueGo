937517d35b57ba3b32cebe6c6af710bf9f1d4da1
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-08-01 15:30:39 +0100

    définir le format de réponse par défaut sur json

--- main.go
+++ main.go
@@ -19,7 +19,6 @@ func main() {
 	// Routes
 	http.HandleFunc("/saluer", saluer)
 	http.HandleFunc("/clients", listerLesClients)
-	http.HandleFunc("/xml/clients", listerLesClientsXML)
 	//  starts server in port 8081
 	fmt.Println("localhost @ "+port)
 	log.Fatal(http.ListenAndServe("localhost"+port, nil))
@@ -35,17 +34,16 @@ func listerLesClients(writer http.ResponseWriter, request *http.Request) {
 		{Name: "John", City: "Paris", Zipcode: "75008"},
 		{Name: "Mary", City: "London", Zipcode: "W1A 0AX"},
 	}
-
-	writer.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(writer).Encode(customers)
-}
-
-func listerLesClientsXML(writer http.ResponseWriter, request *http.Request){
-	customers := []Customer{
-		{Name: "John", City: "Paris", Zipcode: "75008"},
-		{Name: "Mary", City: "London", Zipcode: "W1A 0AX"},
+	jsonHeader := "application/json"
+	xmlHeader := "application/xml"
+	
+	if request.Header.Get("Content-Type") ==  xmlHeader {
+		writer.Header().Set("Content-Type", "application/xml")
+		xml.NewEncoder(writer).Encode(customers)
+	}else{
+		writer.Header().Set("Content-Type", jsonHeader)
+		json.NewEncoder(writer).Encode(customers)
 	}
-
-	writer.Header().Set("Content-Type", "application/xml")
-	xml.NewEncoder(writer).Encode(customers)
 }
+
+
